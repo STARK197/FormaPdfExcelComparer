db2a163f161504cf1ee78db2005ac86502c48bee
STARK197 <aperez@grupoconcretus.com>
2026-06-18 10:20:35 -0500

    Add Excel row model

---
+++ dotnet/FormaPdfExcelComparerDotNet/Models/ExcelRowData.cs
@@ -0,0 +1,8 @@
+namespace FormaPdfExcelComparerDotNet.Models;
+
+public sealed class ExcelRowData
+{
+    public int ExcelRow { get; set; }
+    public object? Evaluacion2 { get; set; }
+    public string? DocumentKey { get; set; }
+}
